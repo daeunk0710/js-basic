6bb90a5589688d82fabf66e138d1a8f8d0a22424
daeunk0710 <thore0710@snu.ac.kr>
2019-12-31 05:02:39 +0900

    #3. Momentum Clone Coding - clock - greeting(updated)

--- momentum/greeting.js
+++ momentum/greeting.js
@@ -11,16 +11,35 @@ function paintGreeting(text){
   greeting.innerText = `Hello ${text}`
 }
 
+function saveName(text){
+  localStorage.setItem(USER_LS, text);
+}
+
+function handleSubmit(event){
+  event.preventDefault(); // 기본 이벤트를 막는다
+  const currentValue = input.value;
+  paintGreeting(currentValue);
+  saveName(currentValue);
+}
+
+function askForName(){
+  form.classList.add(SHOWING_CN);
+  console.log(form.classList);
+  form.addEventListener("submit", handleSubmit);
+}
+
 function loadName(){
   const currentUser = localStorage.getItem(USER_LS);
+  console.log(currentUser);
   if(currentUser === null){
-
+    askForName();
   } else {
     paintGreeting(currentUser);
   }
 }
-function init(){
 
+function init(){
+  loadName();
 }
 
 init();
